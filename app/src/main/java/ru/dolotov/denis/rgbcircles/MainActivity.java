5a264ff6c8260b57320dc099e6a6ba761451f9cb
denis.dolotov <denis.dolotov@gmail.com>
2019-03-02 15:45:25 +0800

    Added: Fullscreen mode

--- app/src/main/java/ru/dolotov/denis/rgbcircles/MainActivity.java
+++ app/src/main/java/ru/dolotov/denis/rgbcircles/MainActivity.java
@@ -1,7 +1,8 @@
 package ru.dolotov.denis.rgbcircles;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         CanvasView canvasView = new CanvasView(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(canvasView);
     }
 }
